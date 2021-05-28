143708bef171b6654d23f65c70f01f6279a30d48
cloudgeeksinc <67391214+cloudgeeksinc@users.noreply.github.com>
2021-05-28 08:04:18 -0400

    merge dev to master

--- Scripts/install.ps1
+++ Scripts/install.ps1
@@ -6,4 +6,11 @@ New-Item -Path C:\Users\WDAGUtilityAccount\.vscode\extensions -Type Directory
 $extensions = @('golang.go','xshrim.txt-syntax')
 $extensions | ForEach-Object { 
     Start-Process PowerShell -WorkingDirectory 'C:\Program Files\Microsoft VS Code\bin' -Wait -ArgumentList "./code --install-extension $_"
+<<<<<<< Updated upstream
     }
+=======
+    }
+
+Install-PackageProvider -Name "NuGet" -Force
+powershell.exe -NoLogo -NoProfile -Command 'Install-Module -Name PackageManagement -MinimumVersion 1.4.6 -Scope CurrentUser -AllowClobber -Confirm:$False -Force'
+>>>>>>> Stashed changes
